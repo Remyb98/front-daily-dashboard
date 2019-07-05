c158954f285a6eff440f99a37e2916b85889c58a
Remy <remy93130@gmail.com>
2019-07-05 19:13:57 +0200

    forget to push mock

---
+++ src/app/dashboard/news-view/mockNews.ts
@@ -0,0 +1,208 @@
+const mockNews = {
+    "business": [
+      {
+        "source": {
+          "id": null,
+          "name": "Lesnumeriques.com"
+        },
+        "author": "Erick Fontaine",
+        "title": "Toyota teste une Prius hybride avec des panneaux solaires - Les Num\u00e9riques",
+        "description": "Une \u00e9nergie propre et in\u00e9puisable",
+        "url": "https:\/\/www.lesnumeriques.com\/voiture\/toyota-teste-prius-hybride-avec-panneaux-solaires-n88993.html",
+        "urlToImage": "https:\/\/dyw7ncnq1en5l.cloudfront.net\/news\/88\/88993\/toyota-prius-solar_1-web.jpg",
+        "publishedAt": "2019-07-05T11:04:00Z",
+        "content": "cette Prius PHEV solaire est capable de parcourir 44,5 km contre 6,1 km pour la version classique.\r\nA batterie \u00e9quivalente (sinon ce seraient de gros escrocs) la version solaire parcours 38km suppl\u00e9mentairesUne voiture \u00e9lectrique avec une batterie de 50kWh at\u2026 [+730 chars]"
+      },
+      {
+        "source": {
+          "id": null,
+          "name": "Frandroid.com"
+        },
+        "author": "Antoine Barbey",
+        "title": "TF1 accuse Molotov de diffuser ses cha\u00eenes sans son accord - FrAndroid",
+        "description": "On savait les relations entre Molotov et TF1 tr\u00e8s difficiles, mais elles viennent de prendre un tout autre tournant avec une nouvelle attaque en justice de la part de TF1, accusant Molotov de proposer ses cha\u00eenes sur son service, sans contrat de diffusion. Si\u2026",
+        "url": "https:\/\/www.frandroid.com\/culture-tech\/juridique\/606879_tf1-accuse-molotov-de-diffuser-ses-chaines-sans-son-accord",
+        "urlToImage": "https:\/\/images.frandroid.com\/wp-content\/uploads\/2017\/04\/6849396_000-par7221549.jpg",
+        "publishedAt": "2019-07-05T11:00:34Z",
+        "content": "Soldes d'\u00e9t\u00e9\r\nPar\r\n Antoine Barbey,\r\n le 05 juillet 2019 \u00e0 13:00\r\nOn savait les relations entre Molotov et TF1 tr\u00e8s difficiles, mais elles viennent de prendre un tout autre tournant avec une nouvelle attaque en justice de la part de TF1, accusant Molotov de p\u2026 [+1843 chars]"
+      },
+      {
+        "source": {
+          "id": "le-monde",
+          "name": "Le Monde"
+        },
+        "author": "Mathilde Damg\u00e9",
+        "title": "Classement des grandes fortunes : peut-on comparer leurs richesses au PIB ? - Le Monde",
+        "description": "DERRI\u00c8RE LE CHIFFRE \u2013 Les classements des grandes fortunes fran\u00e7aises connaissent tous les ans un grand succ\u00e8s, y compris chez les politiques\u2026 mais il faut les prendre avec pr\u00e9caution.",
+        "url": "https:\/\/www.lemonde.fr\/les-decodeurs\/article\/2019\/07\/05\/classement-des-grandes-fortunes-peut-on-comparer-leurs-richesses-au-pib_5485756_4355770.html",
+        "urlToImage": "https:\/\/img.lemde.fr\/2019\/06\/14\/430\/0\/4500\/2250\/1440\/720\/60\/0\/0545b5e_21079ddf47c4442a950b96e80d9414e6-21079ddf47c4442a950b96e80d9414e6-0.jpg",
+        "publishedAt": "2019-07-05T10:43:38Z",
+        "content": "Cest un marronnier, pourrait-on dire, patrimonial\u00a0: les deux principaux classements des grandes fortunes fran\u00e7aises sortent chaque ann\u00e9e en m\u00eame temps, d\u00e9but juillet. M\u00eame si leurs crit\u00e8res varient (Capital, contrairement \u00e0 Challenges, int\u00e8gre les dettes des \u2026 [+4920 chars]"
+      }
+    ],
+    "entertainment": [
+      {
+        "source": {
+          "id": null,
+          "name": "Purepeople.com"
+        },
+        "author": "Marine Corviole",
+        "title": "Anne Vanderlove : La chanteuse de \"Ballade en novembre\" est morte - Pure People",
+        "description": "C'est entour\u00e9e de ses proches qu'Anne Vanderlove est morte chez elle, dans la nuit du 29 au 30 juin 2019. La chanteuse fran\u00e7aise d'origine n\u00e9erlandaise avait 75 ans. R\u00e9v\u00e9l\u00e9e dans les ann\u00e9es 1970, elle \u00e9tait surtout connue pour son interpr\u00e9tation des titres \"B\u2026",
+        "url": "http:\/\/www.purepeople.com\/article\/anne-vanderlove-la-chanteuse-de-ballade-en-novembre-est-morte_a342446\/1",
+        "urlToImage": "https:\/\/static1.purepeople.com\/articles\/6\/34\/24\/46\/@\/4892879-anne-vanderlove-dans-l-emission-les-ann-opengraph_1200-3.jpg",
+        "publishedAt": "2019-07-05T12:13:00Z",
+        "content": "Triste nouvelle pour les fans de la chanson Ballade en novembre. Comme le rapporte l'AFP ce 4 juillet 2019, la chanteuse fran\u00e7aise d'origine n\u00e9erlandaise Anne Vanderlove est morte \u00e0 l'\u00e2ge de 75 ans. \"Anne est morte dans la nuit de samedi \u00e0 dimanche chez nous,\u2026 [+1643 chars]"
+      },
+      {
+        "source": {
+          "id": null,
+          "name": "Purepeople.com"
+        },
+        "author": "Olivia Maunoury",
+        "title": "Laeticia Hallyday en blouse blanche pour une journ\u00e9e \u00e0 l'h\u00f4pital - Pure People",
+        "description": "Quelques jours avant de partir pour plusieurs semaines sur l'\u00eele de Saint-Barth\u00e9lemy avec ses deux filles, Laeticia Hallyday s'est rendue sans elles \u00e0 Paris. La cofondatrice de l'association La Bonne \u00c9toile a pass\u00e9 la journ\u00e9e du 4 juillet 2019 \u00e0 l'h\u00f4pital Arm\u2026",
+        "url": "http:\/\/www.purepeople.com\/article\/laeticia-hallyday-en-blouse-blanche-pour-une-journee-a-l-hopital_a342425\/1",
+        "urlToImage": "https:\/\/static1.purepeople.com\/articles\/5\/34\/24\/25\/@\/4892354-laeticia-hallyday-a-passe-la-journee-du-opengraph_1200-2.jpg",
+        "publishedAt": "2019-07-05T11:51:00Z",
+        "content": "Laeticia Hallyday et H\u00e9l\u00e8ne Darroze ont fond\u00e9 ensemble l'association La Bonne \u00c9toile en janvier 2012 afin de venir en aide aux enfants orphelins ou laiss\u00e9s pour compte, principalement au Vietnam. Les grandes amies ont toutes les deux adopt\u00e9 leurs filles dans \u2026 [+1537 chars]"
+      },
+      {
+        "source": {
+          "id": null,
+          "name": "Lesnumeriques.com"
+        },
+        "author": "Mathieu Chartier",
+        "title": "Netflix va arr\u00eater de fumer\u2026 enfin presque - Les Num\u00e9riques",
+        "description": "Surexposition de la cigarette",
+        "url": "https:\/\/www.lesnumeriques.com\/vie-du-net\/netflix-va-arreter-fumer-enfin-presque-n89003.html",
+        "urlToImage": "https:\/\/dyw7ncnq1en5l.cloudfront.net\/news\/89\/89003\/strangerthings-cigarette-900.jpg",
+        "publishedAt": "2019-07-05T11:32:00Z",
+        "content": "Point\u00e9e du doigt par une association de lutte contre le tabagisme, la tr\u00e8s forte pr\u00e9sence de la cigarette dans les contenus produits par Netflix pousse la plateforme \u00e0 prendre de nouvelles dispositions.\r\n\"Netflix soutient fermement l'expression artistique, (m\u2026 [+2017 chars]"
+      }
+    ],
+    "health": [
+      {
+        "source": {
+          "id": null,
+          "name": "Hitek.fr"
+        },
+        "author": null,
+        "title": "Sida : pour la premi\u00e8re fois, le virus a \u00e9t\u00e9 \u00e9radiqu\u00e9 chez la souris - Hitek.fr",
+        "description": "Pour la premi\u00e8re fois, des chercheurs sont parvenus \u00e0 \u00e9liminer le virus du Sida chez une souris.",
+        "url": "https:\/\/hitek.fr\/actualite\/sida-premiere-fois-virus-eradique-souris_19997",
+        "urlToImage": "https:\/\/hitek.fr\/img\/up_o\/133175419\/hitek_2fb75fc099cbed71b399329a3f707d9f_1562318589.jpeg",
+        "publishedAt": "2019-07-05T09:35:00Z",
+        "content": "V\u00e9ritable fl\u00e9au des temps modernes : environ 172 700 personnes en France sont concern\u00e9s par le VIH, pr\u00e9cise VIH.org. En 2017, ce sont plus de 36, 9 millions de personnes \u00e0 travers le monde qui vivaient avec le VIH. Pr\u00e8s d'un million de d\u00e9c\u00e8s li\u00e9s \u00e0 ce r\u00e9trovi\u2026 [+2206 chars]"
+      },
+      {
+        "source": {
+          "id": "le-monde",
+          "name": "Le Monde"
+        },
+        "author": "Gary Dagorn",
+        "title": "Pourquoi la science tarde encore \u00e0 s'int\u00e9resser aux r\u00e8gles - Le Monde",
+        "description": "#R\u00e8glesNonDites. L\u2019ampleur des probl\u00e8mes de sant\u00e9 li\u00e9s aux r\u00e8gles en fait un r\u00e9el enjeu de sant\u00e9 publique. Pourtant, beaucoup de ces troubles sont encore incompris, la faute \u00e0 une recherche scientifique insuffisante et marqu\u00e9e, elle aussi, par le tabou.",
+        "url": "https:\/\/www.lemonde.fr\/les-decodeurs\/article\/2019\/07\/05\/pourquoi-la-science-tarde-encore-a-s-interesser-aux-regles_5485536_4355770.html",
+        "urlToImage": "https:\/\/img.lemde.fr\/2017\/08\/21\/588\/0\/3500\/1750\/1440\/720\/60\/0\/b7032d8_24010-1vc4548.375app66r.jpg",
+        "publishedAt": "2019-07-05T01:10:00Z",
+        "content": "Article r\u00e9serv\u00e9 aux abonn\u00e9s\r\nLes chiffres sont \u00e9loquents\u00a0: une femme sur\u00a0cinq (soit 370\u00a0millions de personnes) souffrirait de crampes s\u00e9v\u00e8res lors de ses r\u00e8gles\u00a0; une sur\u00a0dix (soit 186\u00a0millions de personnes) souffrirait dendom\u00e9triose, maladie qui touche la mu\u2026 [+2327 chars]"
+      },
+      {
+        "source": {
+          "id": null,
+          "name": "Medisite.fr"
+        },
+        "author": "Emmanuelle Jung, journaliste sant\u00e9",
+        "title": "Moustique tigre : ce qu'il faut absolument faire apr\u00e8s une piq\u00fbre - Medisite",
+        "description": "Le moustique tigre s\u00e9vit en France et il n\u2019est pas pr\u00e8s de plier bagage, d\u2019apr\u00e8s le Pr St\u00e9phane Gayet, infectiologue au CHU de Strasbourg. Comment le reconna\u00eetre ? Et surtout comment r\u00e9agir apr\u00e8s la piq\u00fbre ?",
+        "url": "https:\/\/www.medisite.fr\/piqures-de-moustiques-moustique-tigre-ce-quil-faut-absolument-faire-apres-une-piqure.5519009.719825.html",
+        "urlToImage": "https:\/\/m.mcdn.fr\/files\/images\/article\/9\/0\/0\/5519009\/6775682-inline.jpg",
+        "publishedAt": "2019-07-04T17:14:00Z",
+        "content": "\u00a9 Istock\r\nLe moustique tigre est un moustique tropical qui provient dAsie du Sud Est. Connu scientifiquement sous le nom de \"Aedes albopictus\", il peut transmettre trois virus par sa piq\u00fbre, \u00e0 savoir, le chikungunya, mais aussi la dengue et le virus Zika. \"Sa\u2026 [+3018 chars]"
+      }
+    ],
+    "science": [
+      {
+        "source": {
+          "id": null,
+          "name": "Sciencesetavenir.fr"
+        },
+        "author": "Anne-Sophie Tassart",
+        "title": "Bac 2019 : quelques conseils pour briller lors des oraux de rattrapage - Sciences et Avenir",
+        "description": "Sciences et Avenir donne en vid\u00e9o quelques conseils aux lyc\u00e9ens amen\u00e9s \u00e0 passer les oraux de rattrapage.",
+        "url": "https:\/\/www.sciencesetavenir.fr\/sante\/cerveau-et-psy\/bac-2019-quelques-conseils-pour-briller-lors-des-oraux-de-rattrapage_135266",
+        "urlToImage": "https:\/\/www.sciencesetavenir.fr\/assets\/img\/2019\/07\/05\/cover-r4x3w1000-5d1f2197e6ea3-080-hl-nguyonnet-829179.jpg",
+        "publishedAt": "2019-07-05T10:38:00Z",
+        "content": "C'est le grand jour ! En effet, le 5 juillet 2019 des milliers de lyc\u00e9ens ont pu consulter les r\u00e9sultats du bac 2019 afin de savoir s'ils l'ont obtenu. Dans le cas contraire, pas de panique : obtenir le saint Graal est toujours possible en passant\u00a0des preuves\u2026 [+2319 chars]"
+      },
+      {
+        "source": {
+          "id": null,
+          "name": "Lachainemeteo.com"
+        },
+        "author": "Cyril BONNEFOY",
+        "title": "Demain : les orages sont de retour - La Cha\u00eene M\u00e9t\u00e9o",
+        "description": "Ce premier week-end des vacances commence par un temps lourd et tr&egrave;s orageux. Attention &agrave; vos activit&eacute;s en ext&eacute;rieur sur le centre-est du pays avec un risque de ph&eacute;nom&egrave;nes violents.",
+        "url": "https:\/\/actualite.lachainemeteo.com\/actualite-meteo\/2019-07-05\/demain-les-orages-sont-de-retour-51537",
+        "urlToImage": "https:\/\/static3.mclcm.net\/iod\/images\/v2\/69\/photo\/114\/1200x630_100_300_000000x30x0.jpg?ts=20190705124038",
+        "publishedAt": "2019-07-05T10:30:41Z",
+        "content": "Ce premier week-end des vacances commence par un temps lourd et tr\u00e8s orageux. Attention \u00e0 vos activit\u00e9s en ext\u00e9rieur sur le centre-est du pays avec un risque de ph\u00e9nom\u00e8nes violents.\r\nFrance\r\nLa d\u00e9pression situ\u00e9e au large du Portugal en cette fin de semaine se\u2026 [+2272 chars]"
+      },
+      {
+        "source": {
+          "id": null,
+          "name": "Sciencesetavenir.fr"
+        },
+        "author": "",
+        "title": "Un meurtre commis en Transylvanie il y a 33 000 ans enfin r\u00e9solu ! - Sciences et Avenir",
+        "description": "L'analyse d'un cr\u00e2ne r\u00e9v\u00e8le des traces de coups. La sc\u00e8ne s'est produite il y a plus de 30.000 ans",
+        "url": "https:\/\/www.sciencesetavenir.fr\/archeo-paleo\/paleontologie\/des-preuves-d-un-meurtre-33-000-ans-apres_135222",
+        "urlToImage": "https:\/\/www.sciencesetavenir.fr\/assets\/img\/2019\/07\/04\/cover-r4x3w1000-5d1e131cd0e50-cioclovina.jpg",
+        "publishedAt": "2019-07-05T09:31:00Z",
+        "content": "Cioclovina calvaria est un cr\u00e2ne fossilis\u00e9 retrouv\u00e9 dans une grotte du sud de la Transylvanie, une r\u00e9gion de la Roumanie rendue c\u00e9l\u00e8bre par Bram Stoker qui en fit le lieu de r\u00e9sidence\u00a0de son Dracula. Pas question de vampires ici mais c'est sans doute un meurt\u2026 [+1644 chars]"
+      }
+    ],
+    "technology": [
+      {
+        "source": {
+          "id": null,
+          "name": "Lesnumeriques.com"
+        },
+        "author": "Erick Fontaine",
+        "title": "Toyota teste une Prius hybride avec des panneaux solaires - Les Num\u00e9riques",
+        "description": "Une \u00e9nergie propre et in\u00e9puisable",
+        "url": "https:\/\/www.lesnumeriques.com\/voiture\/toyota-teste-prius-hybride-avec-panneaux-solaires-n88993.html",
+        "urlToImage": "https:\/\/dyw7ncnq1en5l.cloudfront.net\/news\/88\/88993\/toyota-prius-solar_1-web.jpg",
+        "publishedAt": "2019-07-05T11:04:00Z",
+        "content": "cette Prius PHEV solaire est capable de parcourir 44,5 km contre 6,1 km pour la version classique.\r\nA batterie \u00e9quivalente (sinon ce seraient de gros escrocs) la version solaire parcours 38km suppl\u00e9mentairesUne voiture \u00e9lectrique avec une batterie de 50kWh at\u2026 [+730 chars]"
+      },
+      {
+        "source": {
+          "id": null,
+          "name": "Phonandroid.com"
+        },
+        "author": "Florian Bayard",
+        "title": "BianLian : ce malware Android espion enregistre l\u2019\u00e9cran de votre smartphone - Phonandroid",
+        "description": "Attention, le malware Android BianLian cherche \u00e0 voler vos mots de passe en enregistrant l'\u00e9cran de votre smartphone \u00e0 votre insu.",
+        "url": "https:\/\/www.phonandroid.com\/bianlian-malware-android-espion-enregistre-ecran-smartphone.html",
+        "urlToImage": "https:\/\/img.phonandroid.com\/2019\/07\/malware-android-bianlian-enregistre-\u00e9cran-smartphone.jpg",
+        "publishedAt": "2019-07-05T10:15:53Z",
+        "content": "BianLian est un dangereux malware Android destin\u00e9 \u00e0 espionner tout ce que vous faites avec votre smartphone, rapporte une enqu\u00eate de Fortinet. Afin de mettre la main sur des donn\u00e9es sensibles, comme vos mots de passe, vos codes de banque ou vos messages, le l\u2026 [+1860 chars]"
+      },
+      {
+        "source": {
+          "id": null,
+          "name": "Fredzone.org"
+        },
+        "author": "",
+        "title": "Une ville en Floride renvoie un employ\u00e9 apr\u00e8s avoir pay\u00e9 une ran\u00e7on d\u2019une valeur de 460 000 $ \u00e0 des hackers - FREDZONE",
+        "description": "La ville de Lake City en Floride a \u00e9t\u00e9 victime de pirates informatiques le mois de juin dernier. Ces derniers ont introduit un ransomware sophistiqu\u00e9 dans le syst\u00e8me informatique de la ville. Il s\u2019agit d\u2019un logiciel malveillant qui prend en otage les donn\u00e9es \u2026",
+        "url": "http:\/\/www.fredzone.org\/une-ville-en-floride-renvoie-un-employe-apres-avoir-paye-une-rancon-dune-valeur-de-460-000-a-des-hackers-992",
+        "urlToImage": "http:\/\/www.fredzone.org\/wp-content\/uploads\/2019\/07\/Hacking.jpg",
+        "publishedAt": "2019-07-05T09:49:51Z",
+        "content": "La ville de Lake City en Floride a \u00e9t\u00e9 victime de pirates informatiques le mois de juin dernier. Ces derniers ont introduit un ransomware sophistiqu\u00e9 dans le syst\u00e8me informatique de la ville. Il sagit dun logiciel malveillant qui prend en otage les donn\u00e9es pe\u2026 [+2990 chars]"
+      }
+    ]
+  }
+export default mockNews;
