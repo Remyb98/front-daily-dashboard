6eae0ad92d0086c27c25349947d94bd18b4c34c4
Remy <remy93130@gmail.com>
2019-07-09 16:47:06 +0200

    Start the user's connection and the session storage

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,15 +1,42 @@
 import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { HttpClient } from '@angular/common/http';
+import URL from './config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public  jwtHelper: JwtHelperService) { }
+  private user = null;
+
+  constructor(public  jwtHelper: JwtHelperService, private httpClient: HttpClient) { }
 
   public isAuthenticated(): boolean {
-    const token: string = this.jwtHelper.tokenGetter();
-    return !this.jwtHelper.isTokenExpired(token);
+    return !this.jwtHelper.isTokenExpired(this.jwtHelper.tokenGetter());
+  }
+
+  public logout(): void {
+    localStorage.clear();
+  }
+
+  public login(email: string, password: string): void {
+    this.httpClient.post<any[]>(URL + 'users', {email, password})
+      .subscribe(
+        (response: any) => {
+          if (response !== null) {
+            this.user = response;
+            this.setUserSession();
+          }
+        },
+        err => this.user = null
+      );
+  }
+
+  /**
+   * Complete this function when the user interface will be complete
+   */
+  private setUserSession(): void {
+    localStorage.setItem('', '');
   }
 }
